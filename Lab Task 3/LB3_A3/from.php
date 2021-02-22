d7d5c2595fc09172ba5e024258e11265ba2dfaf7
Your Name <you@example.com>
2021-02-22 21:31:36 +0600

    Lab task 3

---
+++ Lab Task 3/LB3_A3/from.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p><h3>Profile Picture</h3></p>
+    <img src="pic/Me.jpg" alt="" width="170" height="200">
+    <br>
+    <form class="" action="upload.php" method="post" enctype="multipart/form-data">
+
+      <input type="file" name="fileToUpload" id="fileToUpload">
+      <br><br>
+      <input type="submit" value="Submit" name="submit">
+
+    </form>
+  </body>
+</html>
