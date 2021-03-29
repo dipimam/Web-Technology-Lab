95ef2f22d25859efa762c6fb6c9ff47b15e5387c
Your Name <you@example.com>
2021-03-29 20:43:06 +0600

    1

---
+++ Final/Lab Task 6/Model/model.php
@@ -0,0 +1,100 @@
+<?php
+
+require_once 'db_connect.php';
+
+
+function showAllProducts(){
+	$conn = db_conn();
+    $selectQuery = 'SELECT * FROM `products` ';
+    try{
+        $stmt = $conn->query($selectQuery);
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $rows;
+}
+
+function showProduct($id){
+	$conn = db_conn();
+	$selectQuery = "SELECT * FROM `products` where ID = ?";
+
+    try {
+        $stmt = $conn->prepare($selectQuery);
+        $stmt->execute([$id]);
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    return $row;
+}
+
+function searchUsername($username){
+    $conn = db_conn();
+    $selectQuery = "SELECT * FROM `studentsinfo` WHERE USERNAME = '$username'";
+
+
+    try{
+        $stmt = $conn->query($selectQuery);
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $rows;
+}
+
+
+function addSignupInfo($data){
+	$conn = db_conn();
+    $selectQuery = "INSERT into studentsinfo (NAME, EMAIL, BIRTH,USERNAME,PASSWORD,GENDER)
+VALUES (:name, :email, :birth, :username, :password, :gender)";
+    try{
+        $stmt = $conn->prepare($selectQuery);
+        $stmt->execute([
+        	':name' => $data['name'],
+        	':email' => $data['email'],
+        	':birth' => $data['birth'],
+					':username' => $data['username'],
+					':password' => $data['password'],
+					':gender' => $data['gender']
+
+          ]);
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
+
+    $conn = null;
+    return true;
+}
+
+
+function updateProduct($id, $data){
+    $conn = db_conn();
+    $selectQuery = "UPDATE products set NAME = ?, BUYING = ?, SELLING = ? where ID = ?";
+    try{
+        $stmt = $conn->prepare($selectQuery);
+        $stmt->execute([
+        	$data['name'], $data['buyingPrice'], $data['sellingPrice'], $id
+        ]);
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
+
+    $conn = null;
+    return true;
+}
+
+function deleteProduct($id){
+	$conn = db_conn();
+    $selectQuery = "DELETE FROM `products` WHERE `ID` = ?";
+    try{
+        $stmt = $conn->prepare($selectQuery);
+        $stmt->execute([$id]);
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
+    $conn = null;
+
+    return true;
+}
